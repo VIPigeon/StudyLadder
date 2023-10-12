9a941ac8834e333553085b032e34e80fe8a65866
VIPigeon <slv.bta@gmail.com>
2023-10-12 17:41:50 +0300

    Lesson 1 result

---
+++ lesson1/input_print.py
@@ -0,0 +1,16 @@
+
+n = int(input())
+print((n % 100) // 10)
+
+# s = input()
+# print(s[-1])
+
+# print(input()[-1])
+
+# print(input()[-2])
+
+
+# print("The next number for the number ", n, " is ", str(n + 1), ".", sep="")
+# print("The previous number for the number ", n, " is ", str(n - 1), ".", sep="")
+# print(f"The next number for the number {n} is {n + 1}.")
+# print(f"The previous number for the number {n} is {n - 1}.")
